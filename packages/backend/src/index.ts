c439247bcc7ece0878f1930204acce415b21bf7c
Tanisha Ahuja <tanishaahuja666@gmail.com>
2025-06-22 16:57:14 +0530

    changes

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -15,6 +15,12 @@ backend.add(import('@backstage/plugin-proxy-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-techdocs-backend'));
+backend.add(import('@backstage/plugin-scaffolder-backend'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
+backend.add(import('@roadiehq/scaffolder-backend-module-http-request'));
+
+
+
 
 // auth plugin
 backend.add(import('@backstage/plugin-auth-backend'));
